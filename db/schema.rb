d145e497ed1ea64d4696df9ff710915717e95304
ezra rush <rushwest@gmail.com>
2016-03-26 02:39:27 +0000

    change db to pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160326004133) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "albums", force: :cascade do |t|
     t.string   "title"
     t.string   "artist"
