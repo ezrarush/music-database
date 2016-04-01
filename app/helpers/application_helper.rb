e0b08a79397b29cf2b22bcb337cd7b94c0dfa158
Ezra Rush <rushwest@gmail.com>
2016-04-01 02:29:28 +0000

    fix filter list for artist lps

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,21 @@
 module ApplicationHelper
   def alphabet_filter(model, parent=nil)
     a = String.new
-    [*('0'..'9'),*('a'..'z')].each do |i|
-      if model.constantize.send(:letter_present?, i)
-        if parent
+    if parent
+      [*('0'..'9'),*('a'..'z')].each do |i|
+        if model.constantize.send(:letter_present?, parent, i)
           a << link_to(i, polymorphic_path([parent, model.constantize], i: i))
-        else
-          a << link_to(i, polymorphic_path(model.constantize, i: i))
+          a << " "
         end
       end
-      a << " "
-    end
-    if parent
       a << link_to('clear', polymorphic_path([parent, model.constantize]))
     else
+      [*('0'..'9'),*('a'..'z')].each do |i|      
+        if model.constantize.send(:letter_present?, i)
+          a << link_to(i, polymorphic_path(model.constantize, i: i))
+          a << " "
+        end
+      end
       a << link_to('clear', polymorphic_path(model.constantize))
     end
     a.html_safe
--- app/models/lp.rb
+++ app/models/lp.rb
@@ -1,3 +1,5 @@
 class Lp < Album
-  
+  def self.letter_present?(parent, letter)
+    parent.lps.where("title ilike ?", letter + '%').count > 0 ? true : false
+  end  
 end
